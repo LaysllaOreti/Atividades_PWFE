bff9590b63d3357fecec99aa022df19bb28e1add
LaysllaOreti <laysllaoreti2006@gmail.com>
2025-08-11 17:00:49 -0300

    Aula do dia 11.08

--- Aula JavaScript - 11_08_2025/Anotações/Exemplo Funções/script.js
+++ Aula JavaScript - 11_08_2025/Anotações/Exemplo Funções/script.js
@@ -17,3 +17,26 @@
 // hello();
 
 //Arrow Function
+
+// let somaArrow = (n1, n2) => n1+n2;
+
+// console.log(somaArrow(3,5));
+
+// let hello = function(){
+//     console.log("Hello");
+//     console.log(somaArrow(7,6));
+// }
+
+// hello();
+
+let btn = document.querySelector("button");
+
+function random(num){
+    return Math.floor(Math.random() * (num + 1));
+}
+
+btn.onclick = function (){
+    let cor = 
+    "rgb(" + random(255) + "," + random(255) + "," + random(255) +")";
+    document.body.style.backgroundColor = cor;
+}
